588056f0743c4a138e9f3802093e87184cb1741a
Alexander Ambrose <alexa@fh-group.com>
2015-02-16 11:51:19 -0500

    Update Ember & Change Route Name

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('modelYear');
+  this.route('model-year');
 });
 
 export default Router;
--- app/transition.js
+++ app/transition.js
@@ -1,10 +1,10 @@
 export default function(){
   this.transition(
     this.toRoute('index'),
-    this.use('fade')
+    this.use('fade', {duration: 5000})
   );
   this.transition(
-    this.toRoute('modelYear'),
-    this.use('fade')
+    this.toRoute('model-year'),
+    this.use('fade', {duration: 5000})
   );
 }
